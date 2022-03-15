2ce0c4a18c67d5e91e383be97fe99e4160669dd7
dmar89 <dmarstoffel@gmail.com>
2022-03-15 18:40:57 -0300

    creando los 3 archivos principales

---
+++ app.js
@@ -0,0 +1,23 @@
+const funcionesDeTareas=require('./funcionesDeTareas')
+const accion = process.argv[2];
+
+switch (accion) {
+    case 'listar':
+        funcionesDeTareas.listar();
+        break;
+    case 'listarPendientes':
+        funcionesDeTareas.listarPendientes();
+        break;
+    case 'crear':
+        let nombre=process.argv[3];
+        let nuevaTarea={titulo:nombre,estado:"pendiente"};
+        funcionesDeTareas.guardarTarea(nuevaTarea);
+        break;
+    case undefined:
+            console.log("Atención - Tienes que pasar una acción");
+        break;
+    default:
+            console.log("No entiendo qué quieres hacer");
+        break;
+ }
+
